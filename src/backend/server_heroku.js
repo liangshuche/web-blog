e459d75f9afc5340a52c4662c2706255e768f8d9
liangshuche <b04901019@ntu.edu.tw>
2018-06-14 14:20:05 +0800

    fix deploy issue

--- src/backend/server_heroku.js
+++ src/backend/server_heroku.js
@@ -15,7 +15,7 @@ const postsSchema = mongoose.Schema({
 
 const Posts = mongoose.model('Posts', postsSchema);
 
-const server = app.listen(5000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
   	console.log('server is running on port 5000');
 });
 
